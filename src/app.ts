5cb217357e2225fe68e1304d78797e89ccd01803
Bablu22 <mdbablu22.bablu22@gmail.com>
2024-10-21 22:22:42 +0600

    Add cors url

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://anaadevelopersltd.com','https://admin.anaadevelopersltd.com'],
     credentials: true,
   }),
 );
