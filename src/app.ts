2cc26cf6fcbdcc396bebb93cf68429b8aebd73a5
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-10-22 10:35:41 +0600

    api-add

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://anaadevelopersltd.com','https://admin.anaadevelopersltd.com/'],
     credentials: true,
     
   }),
